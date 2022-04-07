a57b7f8bdaae43cfbfe531fc03edbd286ebc192d
Nelson Adarmes <jhoeladarmes@gmail.com>
2022-04-07 11:19:41 -0300

    Practica de la clase 5

---
+++ algoritmo.js
@@ -0,0 +1,21 @@
+document.write(`
+    <h1>Sintetizadores antiguos </h1>
+    <p><details>
+        <summary>Synclavier</summary>
+            <p>
+            Sintetizador del 80'
+            </p>
+            <img title= "imagen descriptiva" src=" https://http2.mlstatic.com/D_NQ_NP_650592-MLA31053977222_062019-O.webp" alt="Imagen synclavier">
+            <br> <a href="https://es.wikipedia.org/wiki/Synclavier">Más info</a>
+
+    </details></p> 
+    <details>
+        <summary>Mini moog</summary>
+            <p>
+            Sintetizador de finales del 70'
+            </p>
+                <img title= "imagen descriptiva" src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Minimoog_voyager.jpg" alt="Imagen Mini moog">
+                br> <a href="https://es.wikipedia.org/wiki/Minimoog">Más info</a>
+
+    </details>   
+`);
